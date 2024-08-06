1230309bfc5fbee4128c92d6e0a1a228968a9b2b
JuYoung <jjystar3@gmail.com>
2024-08-06 09:17:34 +0900

    주석 제거

--- src/main/Test.java
+++ src/main/Test.java
@@ -2,120 +2,91 @@ package main;
 
 public class Test {
 
-    public static void printSpiral(int size)
-    {
- 
-        // Create row and col
-        // to traverse rows and columns
-        int row = 0, col = 0;
- 
-        int boundary = size - 1;
-        int sizeLeft = size - 1;
-        Boolean flag = false;
- 
-        // Variable to determine the movement
-        // r = right, l = left, d = down, u = upper
-        char move = 'r';
- 
-        // Array for matrix
-        int matrix[][] = new int[size][size];
- 
-        for (int i = 1; i < size * size + 1; i++) {
- 
-            // Assign the value
-            matrix[row][col] = i;
- 
-            // switch-case to determine the next index
-            switch (move) {
- 
-            // If right, go right
-            case 'r':
-                col += 1;
-                break;
- 
-            // if left, go left
-            case 'l':
-                col -= 1;
-                break;
- 
-            // if up, go up
-            case 'u':
-                row -= 1;
-                break;
- 
-            // if down, go down
-            case 'd':
-                row += 1;
-                break;
-            }
- 
-            // Check if the matrix
-            // has reached array boundary
-            if (i == boundary) {
- 
-                // Add the left size for the next boundary
-                boundary += sizeLeft;
- 
-                // If true rotations has been made,
-                // decrease the size left by 1
-                if (!flag) {
- 
-                    flag = true;
-                }
-                else {
- 
-                    flag = false;
-                    sizeLeft -= 1;
-                }
- 
-                // switch-case to rotate the movement
-                switch (move) {
- 
-                // if right, rotate to down
-                case 'r':
-                    move = 'd';
-                    break;
- 
-                // if down, rotate to left
-                case 'd':
-                    move = 'l';
-                    break;
- 
-                // if left, rotate to up
-                case 'l':
-                    move = 'u';
-                    break;
- 
-                // if up, rotate to right
-                case 'u':
-                    move = 'r';
-                    break;
-                }
-            }
-        }
- 
-        // Print the matrix
-        for (row = 0; row < size; row++) {
-            for (col = 0; col < size; col++) {
- 
-                int n = matrix[row][col];
-                System.out.print((n < 10) ? (" " + n + " ")
-                                          : (n + " "));
-            }
- 
-            System.out.println();
-        }
-    }
- 
-    // Driver Code
-    public static void main(String[] args)
-    {
- 
-        // Get the size of size
-        int size = 5;
- 
-        // Print the Spiral Pattern
-        printSpiral(size);
-    }
+	public static void main(String[] args) {
+
+		int size = 5;
+
+		printSpiral(size);
+	}
+
+	public static void printSpiral(int size) {
+
+		int y = 0, x = 0;
+
+		int boundary = size - 1;
+		int sizeLeft = size - 1;
+		Boolean flag = false;
+
+		char move = 'r';
+
+		int matrix[][] = new int[size][size];
+
+		for (int i = 1; i < size * size + 1; i++) {
+
+			matrix[y][x] = i;
+
+			switch (move) {
+
+			case 'r':
+				x += 1;
+				break;
+
+			case 'l':
+				x -= 1;
+				break;
+
+			case 'u':
+				y -= 1;
+				break;
+
+			case 'd':
+				y += 1;
+				break;
+			}
+
+			if (i == boundary) {
+
+				boundary += sizeLeft;
+
+				if (!flag) {
+
+					flag = true;
+				} else {
+
+					flag = false;
+					sizeLeft -= 1;
+				}
+
+				switch (move) {
+
+				case 'r':
+					move = 'd';
+					break;
+
+				case 'd':
+					move = 'l';
+					break;
+
+				case 'l':
+					move = 'u';
+					break;
+
+				case 'u':
+					move = 'r';
+					break;
+				}
+			}
+		}
+
+		for (y = 0; y < size; y++) {
+			for (x = 0; x < size; x++) {
+
+				int n = matrix[y][x];
+				System.out.print((n < 10) ? (" " + n + " ") : (n + " "));
+			}
+
+			System.out.println();
+		}
+	}
 
 }
